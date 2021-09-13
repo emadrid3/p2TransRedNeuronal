878289431da2940062d650ef7781de056ecaf55b
Emanuel Madrid Restrepo <emanuel7233@gmail.com>
2021-09-12 23:18:30 -0500

    Se añade cambios verificados

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -22,6 +22,6 @@
         <li class="active">
             <a href="{{ url('/historial') }}"><i class="fas fa-history fa-2x" style="float: left; margin: 12px 3px"></i>Historial</a>
         </li>
-        <h6 style="color:white;margin-top:100%;">Version 1.4</h6>
+        <h6 style="color:white;margin-top:100%;">Version 1.5</h6>
     </ul>
 </div>
